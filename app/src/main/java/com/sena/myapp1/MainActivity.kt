7ceccc01fcb3e0e8fe64f64089969df70eabe37a
Verónica Vidal <duberneylopezlopez1@gmail.com>
2024-08-02 21:00:08 -0500

    login

--- app/src/main/java/com/sena/myapp1/MainActivity.kt
+++ app/src/main/java/com/sena/myapp1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.sena.myapp1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btn: Button = findViewById(R.id.button3)
+        btn.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+
+        val tex1: TextView = findViewById(R.id.textView5)
+        tex1.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
--- app/src/main/java/com/sena/myapp1/MainActivity2.kt
+++ app/src/main/java/com/sena/myapp1/MainActivity2.kt
@@ -1,6 +1,8 @@
 package com.sena.myapp1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btn: Button = findViewById(R.id.button5)
+        btn.setOnClickListener{
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
